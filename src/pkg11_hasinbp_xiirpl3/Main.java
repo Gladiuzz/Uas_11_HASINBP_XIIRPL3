5d12e78ad8d9d748718ab39204ea2570852b3f03
Gladiuzz <hasinbasharip@gmail.com>
2019-09-29 08:24:06 +0700

    init commit

---
+++ src/pkg11_hasinbp_xiirpl3/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg11_hasinbp_xiirpl3;
+
+/**
+ *
+ * @author SMKN4
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Splash_Screen first = new Splash_Screen();
+        first.setVisible(true);
+    }
+    
+}
